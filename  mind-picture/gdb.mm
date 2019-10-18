abd6cdf64017ea34a8d74668773fd80e8b15a625
Huanghw <851721619@qq.com>
2019-10-18 10:26:50 +0800

    change name

---  mind-picture/gdb.mm
+++
@@ -1,4 +0,0 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1568941402313" ID="ID_1560256882" MODIFIED="1568941412019" TEXT="gdb"/>
-</map>
---  mind-picture/makefile.mm
+++
@@ -1,4 +0,0 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1568941421978" ID="ID_1774244822" MODIFIED="1568941431401" TEXT="makefile"/>
-</map>
