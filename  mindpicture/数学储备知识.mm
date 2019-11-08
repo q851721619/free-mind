2fdcd9510712187e2f9c9d425768382f83bf0b2d
Huanghw <851721619@qq.com>
2019-11-08 09:28:56 +0800

    +some newfile

---
+++  mindpicture/数学储备知识.mm
@@ -0,0 +1,54 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1572920201635" ID="ID_1021662900" MODIFIED="1572920226698" STYLE="bubble" TEXT="&#x6570;&#x5b66;&#x50a8;&#x5907;&#x77e5;&#x8bc6;">
+<node CREATED="1572920220576" ID="ID_1772297358" MODIFIED="1572920261381" POSITION="right" TEXT="&#x7b49;&#x6bd4;&#x6570;&#x5217;">
+<node CREATED="1572920368175" ID="ID_1067720275" MODIFIED="1572920654335">
+<richcontent TYPE="NODE">&lt;html&gt;
+  &lt;body&gt;
+    &lt;p&gt;
+      &lt;a content=&quot;text/html; charset=utf-8&quot; href=&quot;https://baike.baidu.com/item/%E9%80%9A%E9%A1%B9%E5%85%AC%E5%BC%8F&quot; target=&quot;_blank&quot; http-equiv=&quot;content-type&quot;&gt;通项公式&lt;/a&gt;（等比数列通项公式通过定义式叠乘而来）：&lt;section class=&quot;formula-container formula-container__inline&quot;&gt;
+    &lt;/p&gt;
+    &lt;div style=&quot;text-indent: 28px; width: 89px; display: inline; background-color: rgb(255, 255, 255); white-space: normal&quot; content=&quot;text/html; charset=utf-8&quot; class=&quot;layoutinline formula&quot; http-equiv=&quot;content-type&quot;&gt;
+      &lt;img height=&quot;21&quot; width=&quot;89&quot; align=&quot;absmiddle&quot; title=&quot;&quot; alt=&quot;&quot; src=&quot;https://gss2.bdstatic.com/9fo3dSag_xI4khGkpoWK1HF6hhy/baike/pic/item/7dd98d1001e939016b619cfa75ec54e737d19696.jpg&quot;&gt;
+      
+    &lt;/div&gt;
+    &lt;/section&gt;
+    
+  &lt;/body&gt;
+&lt;/html&gt;
+</richcontent>
+</node>
+<node CREATED="1572920618512" ID="ID_1596789825" MODIFIED="1572920662381">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <div content="text/html; charset=utf-8" class="para" label-module="para" http-equiv="content-type">
+      &#27714;&#21644;&#20844;&#24335;&#65306;<section class="formula-container formula-container__block">
+
+      <div style="float: none; clear: both; width: 110px; display: block; margin-left: 0" class="layoutblock formula">
+        <img height="18" width="110" align="absmiddle" title="" alt="" src="https://gss1.bdstatic.com/9vo3dSag_xI4khGkpoWK1HF6hhy/baike/pic/item/b3b7d0a20cf431ad7ae6c99d4936acaf2edd985a.jpg" />
+        
+      </div>
+      </section>
+      
+    </div>
+    <div class="para" label-module="para">
+      <section class="formula-container formula-container__block">
+      
+
+      <div style="float: none; clear: both; width: 156px; display: block; margin-left: 0" class="layoutblock formula">
+        <img height="40" width="156" align="absmiddle" title="" alt="" src="https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/pic/item/d01373f082025aaf94d1f56ff9edab64024f1ab6.jpg" />
+        
+      </div>
+      </section>
+      
+    </div>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
